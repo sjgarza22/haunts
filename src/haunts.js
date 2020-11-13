bf48e8eee632d5ea76f28c7798ba578916f201fc
sjgarza22 <sjgarza22@gmail.com>
2020-11-13 15:09:29 -0600

    Create Haunts class with contructor and createCard method

---
+++ src/haunts.js
@@ -0,0 +1,32 @@
+class Haunts {
+    constructor(name, description, city, state, rating) {
+        this.name = name;
+        this.description = description;
+        this.city = city;
+        this.state = state;
+        this.rating = rating;
+    }
+
+    createCard(container) {
+        const cardContainer = document.createElement('DIV');
+        const newHauntCard = document.createElement('DIV');
+        const cardBody = document.createElement('DIV');
+        const cardTitle = document.createElement('H5');
+        const cardDescription = document.createElement('P');
+
+        cardContainer.classList.add('col-md-4', 'col-sm-6', 'cell-box');
+        newHauntCard.classList.add("card", "idea-listings");
+        cardBody.classList.add('card-body');
+        cardTitle.classList.add('card-title');
+        cardDescription.classList.add('card-text');
+
+        cardTitle.innerHTML = this.name;
+        cardDescription.innerHTML = this.description;
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardDescription);
+        newHauntCard.append(cardBody);
+        cardContainer.append(newHauntCard);
+        container.append(cardContainer);
+    }
+}
