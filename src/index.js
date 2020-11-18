35198371c9e7ce319711c5c10d449634592a927b
sjgarza22 <sjgarza22@gmail.com>
2020-11-17 20:22:55 -0600

    Fix loginFormHandler, create searchFormHandler, hauntsFetchAll and hauntsFetch functions. Remove test elements

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ const mainUrl = "http://localhost:3000";
 
 document.addEventListener("DOMContentLoaded", () => {
     const login = document.getElementById('login');
-    const loginForm = document.getElementById("login-form");
-    const container = document.getElementById('results');
-
-    loginForm.addEventListener("submit", (e) => loginFormHandler(e))
+    const search = document.getElementById('search-form');
+    const resultsContainer = document.getElementById('results');
 
     function isHidden(hid) {
         if (hid.hidden == true) {
@@ -16,9 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function loginFormHandler(e) {
-        e.preventDefault()
-        const emailInput = e.target.getElementById("login-email").value
-        const pwInput = e.target.getElementById("login-password").value
+        e.preventDefault();
+
+        clearBox('results');
+
+        const emailInput = e.target.querySelector("#login-email").value;
+        const pwInput = e.target.querySelector("#login-password").value;
         loginFetch(emailInput, pwInput)
     }
 
@@ -36,21 +37,50 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(response => response.json())
         .then(json => {
+            localStorage.setItem('jwt_token', json.jwt)
+        })
+    }
+
+    function searchFormHandler(e) {
+        e.preventDefault();
+
+        clearBox('results');
+
+        const searchInput = document.getElementById("search-input").value;
+        //console.log(searchInput);
+        hauntsFetch(searchInput);
+    }
+
+    function hauntsFetchAll() {
+        fetch(mainUrl + "/haunts", {
+            method: "GET",
+            headers: {"Content-Type": "application/json"}
+        })
+        .then(res => res.json())
+        .then(json => {
+            json["data"].forEach(haunt => {
+                console.log(haunt)
+            })
+        })
+    }
+    
+    function hauntsFetch(searchLocation) {
+        const bodyData = { search: { search_input: searchLocation } }
+
+        fetch(mainUrl + "/search", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(bodyData)
+        })
+        .then(response => response.json())
+        .then(json => {
             console.log(json);
         })
     }
 
-    login.addEventListener("click", (e) => {
-        e.preventDefault();
-        loginButton = document.getElementById("form-1");
-        isHidden(loginButton);
-    })
+    hauntsFetchAll();
 
-    let hotel = new Haunts('Haunted Hotel', "This legendary Haunted Hotel has thrilled guests from all over the world.", "Boston", "MA", 5);
-    let asylum = new Haunts('The Asylum', "This infamous Asylum has seen it's fair share of tragedy.", "Here", "Iowa", "3");
-    let bridge = new Haunts('Wailing Bridge', 'Visitors to the Wailing Bridge swear that they have heard a crying woman at night.', 'Denton', 'TX', 4);
-
-    hotel.createCard(container);
-    asylum.createCard(container);
-    bridge.createCard(container);
+    // search.addEventListener("submit", (e) => searchFormHandler(e))
+    login.addEventListener("click", (e) => loginForm(e))
+    aboutLink.addEventListener("click", (e) => aboutPage(e))
 })
