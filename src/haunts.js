5b030492fcc50f2d55bcea2f0e316dcab9d10b39
sjgarza22 <sjgarza22@gmail.com>
2020-11-19 18:41:52 -0600

    Changed rating to ratingTotel. Adding rating and location to Haunt Card.

--- src/haunts.js
+++ src/haunts.js
@@ -1,32 +1,43 @@
 class Haunts {
-    constructor(id, name, description, city, state, rating=0) {
+    constructor(id, name, description, city, state, ratingTotal=0) {
         this.id = id
         this.name = name;
         this.description = description;
         this.city = city;
         this.state = state;
-        this.rating = rating;
+        this.ratingTotal = ratingTotal;
     }
 
     createCard(container) {
+        const ratingSpirit = '<i class="fas fa-ghost"></i>';
+        const horizontalLine = document.createElement('hr');
         const cardContainer = document.createElement('DIV');
         const newHauntCard = document.createElement('DIV');
         const cardBody = document.createElement('DIV');
         const cardTitle = document.createElement('DIV');
+        const rating = document.createElement('P');
+        const location = document.createElement('P');
         const cardDescription = document.createElement('P');
 
         cardContainer.classList.add('col', 'mb-4');
         newHauntCard.classList.add("card", "text-white", "bg-dark", "h-100");
-        //newHauntCard.style.width = '20rem';
         cardBody.classList.add('card-body');
         cardTitle.classList.add('card-header');
         cardDescription.classList.add('card-text');
+        rating.classList.add('card-text', 'text-center');
+        location.classList.add('card-text');
 
-        cardTitle.innerHTML = this.name;
+        cardTitle.innerHTML = `<h5>${this.name}</h5>`;
         cardDescription.innerHTML = this.text_truncate(this.description, 150);
+        rating.innerHTML = `Rating: ${ratingSpirit} ${ratingSpirit} ${ratingSpirit} ${ratingSpirit} ${ratingSpirit} ${this.ratingTotal}`;
+        location.innerHTML = `${this.city}, ${this.state}`;
 
         cardBody.appendChild(cardTitle);
         cardBody.appendChild(cardDescription);
+        cardBody.appendChild(horizontalLine);
+        cardBody.appendChild(rating);
+        cardBody.appendChild(horizontalLine);
+        cardBody.appendChild(location);
         newHauntCard.append(cardTitle);
         newHauntCard.append(cardBody);
         cardContainer.append(newHauntCard);
