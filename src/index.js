e53f8fea2890cee00ab1b10d3371914b8c874e4d
sjgarza22 <sjgarza22@gmail.com>
2020-11-19 21:39:55 -0600

    Moved/removed unneeded functions and variables.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ const mainUrl = "http://localhost:3000";
 
 document.addEventListener("DOMContentLoaded", () => {
     const login = document.getElementById('login');
-    const search = document.getElementById('search-form');
-    const resultsContainer = document.getElementById('results');
-
-    function isHidden(hid) {
-        if (hid.hidden == true) {
-            hid.hidden = false;
-        } else {
-            hid.hidden = true;
-        }
-    }
 
     function loginFormHandler(e) {
         e.preventDefault();
 
-        clearBox('results');
+        clearBox('main');
 
         const emailInput = e.target.querySelector("#login-email").value;
         const pwInput = e.target.querySelector("#login-password").value;
@@ -37,56 +27,30 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(response => response.json())
         .then(json => {
-            localStorage.setItem('jwt_token', json.jwt)
+            // localStorage.getItem('jwt_token')
+            localStorage.setItem('jwt_token', json.jwt);
+            renderNaivation();
         })
     }
 
-    function searchFormHandler(e) {
-        e.preventDefault();
-
-        clearBox('results');
-
-        const searchInput = document.getElementById("search-input").value;
-        //console.log(searchInput);
-        hauntsFetch(searchInput);
-    }
-
-    function hauntsFetchAll() {
-        fetch(mainUrl + "/haunts", {
-            method: "GET",
-            headers: {"Content-Type": "application/json"}
-        })
-        .then(res => res.json())
-        .then(json => {
-            json["data"].forEach(haunt => {
-                console.log(haunt)
-                const newHaunt = new Haunts(haunt['attributes']['id'],
-                                            haunt['attributes']['name'],
-                                            haunt['attributes']['description'],
-                                            haunt['attributes']['haunts_location']['city'],
-                                            haunt['attributes']['haunts_location']['state_abbrev']);
-                newHaunt.createCard(resultsContainer);
-            })
-        })
-    }
-
-    function hauntsFetch(searchLocation) {
-        const bodyData = { search: { search_input: searchLocation } }
-
-        fetch(mainUrl + "/search", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(bodyData)
-        })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-        })
-    }
-
-    hauntsFetchAll();
-
-    // search.addEventListener("submit", (e) => searchFormHandler(e))
-    login.addEventListener("click", (e) => loginForm(e))
-    aboutLink.addEventListener("click", (e) => aboutPage(e))
+    // function hauntsFetchAll() {
+    //     fetch(mainUrl + "/haunts", {
+    //         method: "GET",
+    //         headers: {"Content-Type": "application/json"}
+    //     })
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         json["data"].forEach(haunt => {
+    //             console.log(haunt)
+    //             const newHaunt = new Haunts(haunt['attributes']['id'],
+    //                                         haunt['attributes']['name'],
+    //                                         haunt['attributes']['description'],
+    //                                         haunt['attributes']['haunts_location']['city'],
+    //                                         haunt['attributes']['haunts_location']['state_abbrev']);
+    //             newHaunt.createCard(resultsContainer);
+    //         })
+    //     })
+    // }
+    renderNaivation();
+    loadMain();
 })
