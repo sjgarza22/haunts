93d9675ff9d8051980bed4e3490f9079cd8db5f9
sjgarza22 <sjgarza22@gmail.com>
2020-11-19 21:37:44 -0600

    Create funtion to renderNavigation that renders a specific navigation depending on whether a user is logged in.

---
+++ src/nav.js
@@ -0,0 +1,32 @@
+function renderNaivation() {
+    const navLinksContainer = document.getElementById('nav-links');
+
+    if (localStorage.jwt_token) {
+        navLinksContainer.innerHTML = `<li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="about">About</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="contact">Contact</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="sign-out">Sign Out</a>
+                                        </li>`;
+        document.getElementById('sign-out').addEventListener("click", () => { localStorage.removeItem('jwt_token') });
+    } else {
+        navLinksContainer.innerHTML = `<li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="about">About</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="contact">Contact</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="login">Login</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link m-2 menu-item" id="sign-up">Sign Up</a>
+                                        </li>`;
+        
+        document.getElementById('login').addEventListener("click", (e) => loginForm(e))
+    }
+    document.getElementById('about').addEventListener("click", (e) => aboutPage(e))
+}
