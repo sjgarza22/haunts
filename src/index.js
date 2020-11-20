5981b028cab8e03ee3034f92d1aa4f5460c5d4af
sjgarza22 <sjgarza22@gmail.com>
2020-11-20 00:33:10 -0600

    Move loginFormHandler and loginFetch to static file.

--- src/index.js
+++ src/index.js
@@ -3,36 +3,6 @@ const mainUrl = "http://localhost:3000";
 document.addEventListener("DOMContentLoaded", () => {
     const login = document.getElementById('login');
 
-    function loginFormHandler(e) {
-        e.preventDefault();
-
-        clearBox('main');
-
-        const emailInput = e.target.querySelector("#login-email").value;
-        const pwInput = e.target.querySelector("#login-password").value;
-        loginFetch(emailInput, pwInput)
-    }
-
-    function loginFetch(email, password){
-        const bodyData = {user: {
-                email: email,
-                password: password
-            }
-        }
-
-        fetch(mainUrl + "/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(bodyData)
-        })
-        .then(response => response.json())
-        .then(json => {
-            // localStorage.getItem('jwt_token')
-            localStorage.setItem('jwt_token', json.jwt);
-            renderNaivation();
-        })
-    }
-
     // function hauntsFetchAll() {
     //     fetch(mainUrl + "/haunts", {
     //         method: "GET",
@@ -51,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //         })
     //     })
     // }
+
     renderNaivation();
     loadMain();
 })
